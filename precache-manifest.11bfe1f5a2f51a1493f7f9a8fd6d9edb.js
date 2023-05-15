b5cddc6cd68b7c16ce2c3dfab7982dcaea9945d9
rei0000 <hola131098@gmail.com>
2023-05-15 04:25:13 +0400

    initial commit

---
+++ precache-manifest.11bfe1f5a2f51a1493f7f9a8fd6d9edb.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "dafd5ee5589b4e8bdddb",
+    "url": "/static/css/main.ee5e2211.chunk.css"
+  },
+  {
+    "revision": "dafd5ee5589b4e8bdddb",
+    "url": "/static/js/main.dafd5ee5.chunk.js"
+  },
+  {
+    "revision": "37b0f16ec4f061202b94",
+    "url": "/static/js/1.37b0f16e.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "d106ab970fa392838493e376cead531a",
+    "url": "/index.html"
+  }
+];
